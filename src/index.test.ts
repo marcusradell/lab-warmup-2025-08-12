ce5bff503103a3d86cac0d69ac9e39cc83c0fb1a
Marcus Rådell <marcus@radell.net>
2025-08-19 12:18:00 +0200

    remove basic test

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { deepStrictEqual } from "assert";
-import { test, describe } from "node:test";
-
-describe("Basic test", () => {
-  test("should assert that 1 is deeply equal to 1", () => {
-    deepStrictEqual(1, 1);
-  });
-});
